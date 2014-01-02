0f18f30df767ad48a90c76fd1e00c7d8a0d8def2
patxi <patxi.gortazar@gmail.com>
2014-01-02 23:20:26 +0100

    Solver problem with mysql from travis environment

--- prepare_solo_rb.sh
+++ prepare_solo_rb.sh
@@ -5,7 +5,7 @@ $PWD = `pwd`
 rm ./solo.rb
 echo file_cache_path \"$PWD\" > solo.rb
 echo cookbook_path \"$PWD/cookbooks\" >> solo.rb
-echo json_attribs \"$PWD/node.json\" >> solo.rb
+echo json_attribs \"$PWD/node-travis.json\" >> solo.rb
 
 echo "solo.rb contents:"
 cat solo.rb
