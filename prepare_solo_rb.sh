2c7e724d60b9aa6823d63d2422b43dadbbb32c8a
patxi <patxi.gortazar@gmail.com>
2014-01-02 16:57:30 +0100

    Fixed solo.rb format

--- prepare_solo_rb.sh
+++ prepare_solo_rb.sh
@@ -3,9 +3,9 @@
 $PWD = `pwd`
 
 rm ./solo.rb
-echo file_cache_path "$PWD" > solo.rb
-echo cookbook_path "$PWD/cookbooks" >> solo.rb
-echo json_attribs "$PWD/node.json" >> solo.rb
+echo file_cache_path \"$PWD\" > solo.rb
+echo cookbook_path \"$PWD/cookbooks\" >> solo.rb
+echo json_attribs \"$PWD/node.json\" >> solo.rb
 
 echo "solo.rb contents:"
 cat solo.rb
